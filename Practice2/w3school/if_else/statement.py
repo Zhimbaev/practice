85a874b0aa4edded491c3963d46fb2ce1531d2a8
Zamir <z_zhimbaev@kbtu.kz>
2026-02-07 12:40:05 +0500

    statement examples

---
+++ Practice2/w3school/if_else/statement.py
@@ -0,0 +1,28 @@
+#ex1
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+
+#ex2
+number = 15
+if number > 0:
+  print("The number is positive")
+
+#ex3
+age = 20
+if age >= 18:
+  print("You have full legal rights")
+
+
+#ex4
+is_logged_in = True
+if is_logged_in:
+  print("Welcome back!")
+
+#ex5
+a = 7
+b = 7
+
+if a == b:
+    print("a равно b")
