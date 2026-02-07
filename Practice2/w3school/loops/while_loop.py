1b8e9b0e073d9f302b25cfda18a25d51ecb64834
Zamir <z_zhimbaev@kbtu.kz>
2026-02-07 12:48:14 +0500

    while loop examples

---
+++ Practice2/w3school/loops/while_loop.py
@@ -0,0 +1,26 @@
+#ex1
+i = 1
+while i <= 3:
+    print("Loading... Step", i)
+    i += 1
+
+#ex2
+n = 1
+while n < 10:
+    print("Value:", n)
+    n *= 2
+#ex3
+battery = 2
+while battery > 0:
+    print("Phone is ON. Battery:", battery)
+    battery -= 1
+#Ex4
+timer = 3
+while timer > 0:
+    print("Wait...")
+    timer -= 1
+#ex5
+text = ""
+while len(text) < 3:
+    text += "A"
+    print("String:", text)
