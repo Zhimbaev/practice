9ab3d16eb6e1eb6070e18bd76b175bba616cf94d
Zamir <z_zhimbaev@kbtu.kz>
2026-02-07 12:34:23 +0500

    value examples

---
+++ Practice2/w3school/boolean/values.py
@@ -0,0 +1,21 @@
+#ex1
+print(3 < 5)
+
+#ex2
+print(bool(""))
+
+#ex3
+x = 10
+y = 10
+print(x == y)
+
+#ex4
+numbers = []
+print(bool(numbers))
+
+#ex5
+a = 5
+if a:
+    print("True")
+else:
+    print("False")
