e113c9b71543cfbcfb9ff1b46faf74d96fbad0fc
Zamir <z_zhimbaev@kbtu.kz>
2026-02-07 12:49:52 +0500

    for loop examples

---
+++ Practice2/w3school/loops/for.py
@@ -0,0 +1,20 @@
+#ex1
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
+#ex2
+for x in "banana":
+  print(x)
+
+#ex3
+for i in range(3):
+    print("Num", i)
+
+#ex4
+for i in range(10, 13):
+    print("Level:", i)
+
+#ex5
+for i in range(0, 10, 5):
+    print("Step", i)
